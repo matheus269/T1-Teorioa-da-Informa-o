d546331e5cd8821bfb5ca12578040920e54fb4e4
Pedro Viegas <pedroviegas96@gmail.com>
2021-10-06 19:49:32 -0300

    Adicionar tratamento de exceções nos controllers

--- codificador-master/codificador-master/src/main/java/br/unisinos/teoria/computacao/controller/EncoderController.java
+++ codificador-master/codificador-master/src/main/java/br/unisinos/teoria/computacao/controller/EncoderController.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,42 +22,53 @@ public class EncoderController {
 
 	public static final String SUFIX_ARCHIVE_ENCODE = ".cod";
 	public static final String SUFIX_ARCHIVE_DECODE = ".dec";
-	
+
 	@Autowired
 	EncodeService encodeService;
 
 	@PostMapping(value = "/encoder/{tipoEncode}")
-	public ResponseEntity<String> encode(
-			@PathVariable("tipoEncode") Integer tipoEncode,
-			@RequestHeader("file") String filePath, 
-			@RequestHeader("divisor") Integer divisor) throws IOException {
-
-		TipoCodificacao tipoCodificacao = TipoCodificacao.parse(tipoEncode);
-		if (TipoCodificacao.G.equals(tipoCodificacao)) {
-			if (divisor == null) {
-				throw new RuntimeException();
+	public ResponseEntity<String> encode(@PathVariable("tipoEncode") Integer tipoEncode,
+			@RequestHeader("file") String filePath, @RequestHeader("divisor") Integer divisor) {
+		try {
+			TipoCodificacao tipoCodificacao = TipoCodificacao.parse(tipoEncode);
+			if (TipoCodificacao.G.equals(tipoCodificacao)) {
+				if (divisor == null) {
+					throw new RuntimeException();
+				}
 			}
+
+			byte[] data = Files.readAllBytes(Paths.get(filePath));
+			byte[] response = encodeService.encodar(data, divisor, tipoCodificacao);
+
+			int extIndex = filePath.lastIndexOf(".");
+			String newPath = (extIndex > -1 ? filePath.substring(0, extIndex) : filePath) + SUFIX_ARCHIVE_ENCODE;
+			Files.write(Paths.get(newPath), response);
+
+			return ResponseEntity.ok("Arquivo codificado disponível em: " + newPath);
+
+		} catch (IOException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Caminho do arquivo inválido");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno");
 		}
-
-		byte[] data =  Files.readAllBytes(Paths.get(filePath));
-		byte[] response = encodeService.encodar(data, divisor, tipoCodificacao);
-		 
-		int extIndex = filePath.lastIndexOf(".");
-		String newPath = (extIndex > -1 ? filePath.substring(0, extIndex) : filePath) + SUFIX_ARCHIVE_ENCODE;
-		Files.write(Paths.get(newPath), response);
-		
-		return ResponseEntity.ok("Arquivo codificado disponível em: " + newPath);
-
 	}
 
 	@PostMapping(value = "/decoder")
 	public ResponseEntity<String> decode(@RequestHeader("file") String filePath) throws IOException {
-		byte[] data = Files.readAllBytes(Paths.get(filePath));
-		byte[] response = encodeService.decode(data);
-		
-		int extIndex = filePath.lastIndexOf(".");
-		String newPath = (extIndex > -1 ? filePath.substring(0, extIndex) : filePath) + SUFIX_ARCHIVE_DECODE;
-		Files.write(Paths.get(newPath), response);
-		return ResponseEntity.ok("Arquivo decodificado disponível em: " + newPath);
+		try {
+			byte[] data = Files.readAllBytes(Paths.get(filePath));
+			byte[] response = encodeService.decode(data);
+
+			int extIndex = filePath.lastIndexOf(".");
+			String newPath = (extIndex > -1 ? filePath.substring(0, extIndex) : filePath) + SUFIX_ARCHIVE_DECODE;
+			Files.write(Paths.get(newPath), response);
+
+			return ResponseEntity.ok("Arquivo decodificado disponível em: " + newPath);
+
+		} catch (IOException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Caminho do arquivo inválido");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno");
+		}
 	}
 }
--- codificador-master/codificador-master/src/main/java/br/unisinos/teoria/computacao/controller/NoiseTreatmentController.java
+++ codificador-master/codificador-master/src/main/java/br/unisinos/teoria/computacao/controller/NoiseTreatmentController.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -17,32 +18,50 @@ import br.unisinos.teoria.computacao.service.NoiseTreatmentService;
 @RestController
 @RequestMapping("/noise-treatment")
 public class NoiseTreatmentController {
-	
+
 	public static final String SUFIX_ARCHIVE_ENCODE = ".ecc";
 	public static final String SUFIX_ARCHIVE_DECODE = ".cod";
 
-	@Autowired	
+	@Autowired
 	private NoiseTreatmentService service;
-	
+
 	@PostMapping(value = "/encoder")
-	public ResponseEntity<String> encode(@RequestHeader("file") String filePath) throws IOException{
-		byte[] data =  Files.readAllBytes(Paths.get(filePath));
-        byte[] result = service.addNoiseTreatment(data);
-                
-        int extIndex = filePath.lastIndexOf(".");
-        String newPath = (extIndex > -1 ? filePath.substring(0, extIndex) : filePath) + SUFIX_ARCHIVE_ENCODE;
-        Files.write(Paths.get(newPath), result);
-		return ResponseEntity.ok("Arquivo codificado disponível em: " + newPath);
+	public ResponseEntity<String> encode(@RequestHeader("file") String filePath) {
+		try {
+			byte[] data = Files.readAllBytes(Paths.get(filePath));
+			byte[] result = service.addNoiseTreatment(data);
+
+			int extIndex = filePath.lastIndexOf(".");
+			String newPath = (extIndex > -1 ? filePath.substring(0, extIndex) : filePath) + SUFIX_ARCHIVE_ENCODE;
+			Files.write(Paths.get(newPath), result);
+			
+			return ResponseEntity.ok("Arquivo codificado disponível em: " + newPath);
+			
+		} catch (IOException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Caminho do arquivo inválido");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno");
+		}
 	}
-	
+
 	@PostMapping(value = "/decoder")
-	public ResponseEntity<String> decode(@RequestHeader("file") String filePath) throws IOException, InvalidCRC{
-		byte[] data =  Files.readAllBytes(Paths.get(filePath));
-        byte[] result = service.checkNoiseTreatment(data);
-        
-        int extIndex = filePath.lastIndexOf(".");
-        String newPath = (extIndex > -1 ? filePath.substring(0, extIndex) : filePath) + SUFIX_ARCHIVE_DECODE;
-        Files.write(Paths.get(newPath), result);
-		return ResponseEntity.ok("Arquivo decodificado disponível em: " + newPath);
+	public ResponseEntity<String> decode(@RequestHeader("file") String filePath) throws InvalidCRC {
+		try {
+			byte[] data = Files.readAllBytes(Paths.get(filePath));
+			byte[] result = service.checkNoiseTreatment(data);
+
+			int extIndex = filePath.lastIndexOf(".");
+			String newPath = (extIndex > -1 ? filePath.substring(0, extIndex) : filePath) + SUFIX_ARCHIVE_DECODE;
+			Files.write(Paths.get(newPath), result);
+			
+			return ResponseEntity.ok("Arquivo decodificado disponível em: " + newPath);
+			
+		} catch (InvalidCRC e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		} catch (IOException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Caminho do arquivo inválido");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno");
+		}
 	}
 }
